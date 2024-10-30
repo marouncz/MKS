4ce54d4dc0232a1e63c6e2e6dd6dbb099d157e29
marouncz <marouncz@gmail.com>
2024-10-30 08:43:44 +0100

    cv06 ds18b20 read

---
+++ CV06/Core/Inc/sct.h
@@ -0,0 +1,22 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 9, 2024
+ *      Author: 240598
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+void sct_init(void);
+
+void sct_led(uint32_t value);
+
+void sct_value(uint16_t value, uint8_t led, uint8_t decimalPlace);
+
+
+
+
+#endif /* INC_SCT_H_ */
+
+
