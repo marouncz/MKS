27e473f5fd53819272757ae9694143a277b6bab9
marouncz <marouncz@gmail.com>
2024-12-11 08:34:23 +0100

    http web server

--- CV12/Core/Src/main.c
+++ CV12/Core/Src/main.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "lwip/apps/httpd.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -303,6 +303,7 @@ void StartDefaultTask(void const * argument)
   MX_LWIP_Init();
   /* USER CODE BEGIN 5 */
   tcpecho_init();
+  httpd_init();
   /* Infinite loop */
   for(;;)
   {
